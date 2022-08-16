7893d7814ba6e6d61a2da83e8971ad7978abb6fb
Dave Goosem <davidgoosem@aceik.com.au>
2022-08-16 02:05:03 +0000

    Facebook demopage with campaign link

---
+++ JSS/xm-personalise-demo/src/pages/Facebook.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+import { getPublicUrl } from '@sitecore-jss/sitecore-jss-nextjs';
+
+// Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
+// If you're not supporting the Experience Editor, you can remove this.
+const publicUrl = getPublicUrl();
+/**
+ * Rendered for 500 errors on both server and client. Used only in Production mode.
+ * @link https://nextjs.org/docs/advanced-features/custom-error-page#more-advanced-error-page-customizing
+ */
+const FacebookPage: NextPage = () => (
+    <>
+        <Head>
+            <title>Facebook | Ally David </title>
+
+            <link rel="stylesheet" href={`${publicUrl}/bootstrap-3.3.7/css/bootstrap.min.css`} />
+        </Head>
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-12">
+                    <a href={`${publicUrl}/?campaign="IslandCompetition"`}>
+                        <img
+                            src={`${publicUrl}/AllyDavid_DemoFacebook.jpg`}
+                            className="mx-auto d-block"
+                        ></img>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </>
+);
+
+export default FacebookPage;
